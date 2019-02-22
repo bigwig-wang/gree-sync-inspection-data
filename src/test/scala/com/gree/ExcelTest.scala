24439530b24c3485210975cc33586f7c61f025e0
wei.qi <wei.qi@thoughtworks.com>
2019-02-22 16:18:50 +0800

    add test file

--- src/test/scala/com/gree/ExcelTest.scala
+++ src/test/scala/com/gree/ExcelTest.scala
@@ -2,14 +2,14 @@ package com.gree
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{StructType, _}
-import org.scalatest.{BeforeAndAfter, FlatSpec}
-L
+import org.scalatest._
 
-class ExcelTest extends FlatSpec with BeforeAndAfter {
+
+class ExcelTest extends FlatSpec with BeforeAndAfter with GivenWhenThen {
 
   var spark: SparkSession = _
 
-  val path = "/Users/wei.qi/workspace/tw_work/gree/gree-sync-inspection-data/data/科室物料组对应表s.xlsx"
+  val path = "/Users/wei.qi/workspace/tw_work/gree/gree-sync-inspection-data/data/科室物料组对应表.xlsx"
 
   val customSchema = StructType(Array(
     StructField("Keshi", StringType, nullable = false),
@@ -29,7 +29,6 @@ class ExcelTest extends FlatSpec with BeforeAndAfter {
   it should "get data from file" in {
     val service = SparkExcelUtil.apply
     val df = service.read(spark, path)
-    df.show()
     val count = df.count()
     assert(count > 0)
   }
@@ -37,7 +36,6 @@ class ExcelTest extends FlatSpec with BeforeAndAfter {
   it should "get data from file by customSchema" in {
     val service = SparkExcelUtil.apply
     val df = service.read(spark, path, customSchema)
-    df.show()
     val count = df.count()
     assert(count > 0)
   }
@@ -45,7 +43,6 @@ class ExcelTest extends FlatSpec with BeforeAndAfter {
   it should "get data from file with sheet" in {
     val service = SparkExcelUtil.apply
     val df = service.read(spark, path, "'珠海总部科室编码'!")
-    df.show()
     val count = df.count()
     assert(count > 0)
   }
@@ -53,9 +50,22 @@ class ExcelTest extends FlatSpec with BeforeAndAfter {
   it should "get data from file with sheet and schema" in {
     val service = SparkExcelUtil.apply
     val df = service.read(spark, path, "'珠海总部科室编码'!A2",customSchema)
-    df.show()
     val count = df.count()
     assert(count > 0)
+  }
+
+  it should "get data from broadcast " in {
+    val service = SparkExcelUtil.apply
+    val df = service.read(spark, path, "'珠海总部科室编码'!A2",customSchema)
+    val count = df.count()
+
+    When("count greater than 0")
+    assert(count > 0)
+
+    Then("broadcast")
+    val broadcastVar = spark.sparkContext.broadcast(df)
+
+    broadcastVar.value.show()
   }
 
   after {
